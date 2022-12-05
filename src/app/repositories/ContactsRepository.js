e3611bc518668830e72684733fd0df3bfb64d7ed
Eduardo <eduardo@conectra.com.br>
2022-12-05 13:50:59 -0300

    feat: adiciona rota de delecao de contato

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,19 +1,37 @@
-const { uuid } = require('uuidv4');
+const { v4 } = require('uuid');
 
-const contacts = [
+let contacts = [
 	{
-		id: uuid(),
+		id: v4(),
 		name: 'Eduardo',
 		email: 'eduardoknopp7@gmail.com',
 		phone: '47996264041',
-		category_id: uuid()
+		category_id: v4()
+	},
+	{
+		id: v4(),
+		name: 'Jose',
+		email: 'jose@gmail.com',
+		phone: '47988726666',
+		category_id: v4()
 	},
 ];
 
 class ContactRepository {
 	findAll() {
+		return new Promise((resolve) => resolve(contacts));
+	}
+
+	findById(id) {
+		return new Promise((resolve) => resolve(
+			contacts.find((contact) => contact.id === id)
+		));
+	}
+
+	delete(id) {
 		return new Promise((resolve) => {
-			resolve(contacts);
+			contacts = contacts.filter((contact) => contact.id !== id);
+			resolve();
 		});
 	}
 }
