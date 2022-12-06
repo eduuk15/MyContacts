83a19b462a03427d6b269b0a6b76a327685827da
Eduardo <eduardo@conectra.com.br>
2022-12-06 15:37:31 -0300

    refactor: refatora metodo de update

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -36,13 +36,6 @@ class ContactRepository {
 		return row;
 	}
 
-	delete(id) {
-		return new Promise((resolve) => {
-			contacts = contacts.filter((contact) => contact.id !== id);
-			resolve();
-		});
-	}
-
 	async create({ name, email, phone, category_id }) {
 		const [row] = await db.query(`
 			INSERT INTO contacts(name, email, phone, category_id)
@@ -53,21 +46,20 @@ class ContactRepository {
 		return row;
 	}
 
-	update(id, { name, email, phone, category_id }) {
+	async update(id, { name, email, phone, category_id }) {
+		const [row] = await db.query(`
+			UPDATE contacts
+			SET name = $1, email = $2, phone = $3, category_id = $4
+			WHERE id = $5
+			RETURNING *
+		`, [name, email, phone, category_id, id]);
+		return row;
+	}
+
+	delete(id) {
 		return new Promise((resolve) => {
-			const updatedContact = {
-				id,
-				name,
-				email,
-				phone,
-				category_id
-			};
-
-			contacts = contacts.map((contact) => (
-				contact.id === id ? updatedContact : contact
-			));
-
-			resolve(updatedContact);
+			contacts = contacts.filter((contact) => contact.id !== id);
+			resolve();
 		});
 	}
 }
